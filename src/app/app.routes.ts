f568ae41e02228c88a86333448ce1752deb6cd00
000-dir-wmi-0001 <momindy321@gmail.com>
2025-02-20 17:25:04 +0530

    thurs last commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './auth/login/login.component';
 import { HomepageComponent } from './auth/homepage/homepage.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoggedInAuthGuard } from './guards/logged-in-auth.guard';
+import { ForgotPassComponent } from './auth/forgot-pass/forgot-pass.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
@@ -11,7 +13,12 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent, canActivate: [LoggedInAuthGuard] },
       { path: 'homepage', component: HomepageComponent },
       { path: "register", component: RegisterComponent, canActivate: [LoggedInAuthGuard] },
-      { path: 'password_reset', component: for },
+      { path: 'password_reset', component: ForgotPassComponent },
+
     ]
   },
+  {path:'dashboard', component:DashboardComponent,children:[
+    
+  ]
+},
 ]
