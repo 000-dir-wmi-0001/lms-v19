e732a208d8f8ae6cc04e09d114ec03b03b4a1584
000-dir-wmi-0001 <momindy321@gmail.com>
2025-02-24 13:11:59 +0530

    route fix

--- src/app/dashboard/module/module.component.ts
+++ src/app/dashboard/module/module.component.ts
@@ -10,7 +10,7 @@ import {
 } from '@angular/forms';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { FeeService } from '../../services/fee.service';
 
@@ -21,92 +21,92 @@ import { ReceiptService } from '../../services/receipt.service';
 import { provideHttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-module',
-  imports: [CommonModule, RouterLink, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, RouterLink, RouterOutlet, ReactiveFormsModule, FormsModule],
   templateUrl: './module.component.html',
   styleUrl: './module.component.css'
 })
 export class ModuleComponent {
-  name: any;
-  studentList: any = [];
-  isLoading = true;
-  // modalRef!: BsModalRef;
-  bsModalRef!: import('ngx-bootstrap/modal').BsModalRef<unknown>;
-  msgText: any;
-  toWAnumber: any;
-  listTitles = [
-    { name: 'index' },
-    { name: 'studentName' },
-    // { name: 'Mobile no. '},
-    // { name: 'totalFee' },
-    // { name: 'pendingFee' },
-    { name: 'Details' },
-    { name: 'Action' },
-  ];
+  // name: any;
+  // studentList: any = [];
+  // isLoading = true;
+  // // modalRef!: BsModalRef;
+  // bsModalRef!: import('ngx-bootstrap/modal').BsModalRef<unknown>;
+  // msgText: any;
+  // toWAnumber: any;
+  // listTitles = [
+  //   { name: 'index' },
+  //   { name: 'studentName' },
+  //   // { name: 'Mobile no. '},
+  //   // { name: 'totalFee' },
+  //   // { name: 'pendingFee' },
+  //   { name: 'Details' },
+  //   { name: 'Action' },
+  // ];
 
-  constructor(
-    private feeService: FeeService,
-    private storageService: StorageService,
-    private modalService: BsModalService,
-    private router: Router,
-    private userService: UserService
-  ) { }
+  // constructor(
+  //   private feeService: FeeService,
+  //   private storageService: StorageService,
+  //   private modalService: BsModalService,
+  //   private router: Router,
+  //   private userService: UserService
+  // ) { }
 
-  ngOnInit(): void {
-    this.getStudents();
-  }
-  getStudents() {
-    this.feeService.getStudentUser().subscribe({
-      next: (data: any) => {
-        this.isLoading = false;
-        this.studentList = data.studentUsers;
-        console.log("data", data.studentUsers);
-        console.log("thisone", this.studentList);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
-  }
+  // ngOnInit(): void {
+  //   this.getStudents();
+  // }
+  // getStudents() {
+  //   this.feeService.getStudentUser().subscribe({
+  //     next: (data: any) => {
+  //       this.isLoading = false;
+  //       this.studentList = data.studentUsers;
+  //       console.log("data", data.studentUsers);
+  //       console.log("thisone", this.studentList);
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
+  //     },
+  //   });
+  // }
 
-  handleDoubleClick(i: any) {
-    this.router.navigate(['/dashboard/fee/displayStudentInfo/', i])
-  }
+  // handleDoubleClick(i: any) {
+  //   this.router.navigate(['/dashboard/fee/displayStudentInfo/', i])
+  // }
 
-  searchStudent() {
-    if (!this.name) {
-      this.getStudents();
-    } else {
-      this.studentList = this.studentList.filter((student: any) => {
-        console.log(student)
-        return student.user?.firstName?.toLowerCase().includes(this.name.toLowerCase())
-      }
-      );
-    }
-  }
+  // searchStudent() {
+  //   if (!this.name) {
+  //     this.getStudents();
+  //   } else {
+  //     this.studentList = this.studentList.filter((student: any) => {
+  //       console.log(student)
+  //       return student.user?.firstName?.toLowerCase().includes(this.name.toLowerCase())
+  //     }
+  //     );
+  //   }
+  // }
 
-  editFee(id: any) {
-    console.log(id);
-    this.router.navigate(['dashboard/fee/edit', id]);
-  }
+  // editFee(id: any) {
+  //   console.log(id);
+  //   this.router.navigate(['dashboard/fee/edit', id]);
+  // }
 
-  writeReminderMsg(template: TemplateRef<any>, id: any) {
-    this.bsModalRef = this.modalService.show(template);
-    this.userService.getUser(id).subscribe({
-      next: (data: any) => {
-        this.toWAnumber = data.user.mobileno;
-        console.log(this.toWAnumber);
+  // writeReminderMsg(template: TemplateRef<any>, id: any) {
+  //   this.bsModalRef = this.modalService.show(template);
+  //   this.userService.getUser(id).subscribe({
+  //     next: (data: any) => {
+  //       this.toWAnumber = data.user.mobileno;
+  //       console.log(this.toWAnumber);
 
-      },
-      error: (err) => {
-        console.log(err)
-      }
-    })
-  }
+  //     },
+  //     error: (err) => {
+  //       console.log(err)
+  //     }
+  //   })
+  // }
 
-  sendReminder(text: any) {
-    this.msgText = text;
-    const whatsappUrl = `https://wa.me/${this.toWAnumber}?text=${encodeURIComponent(this.msgText)}`;
-    // Redirect the user to the WhatsApp URL
-    window.location.href = whatsappUrl;
-  }
+  // sendReminder(text: any) {
+  //   this.msgText = text;
+  //   const whatsappUrl = `https://wa.me/${this.toWAnumber}?text=${encodeURIComponent(this.msgText)}`;
+  //   // Redirect the user to the WhatsApp URL
+  //   window.location.href = whatsappUrl;
+  // }
 }
